3ff3c0740ddad05db8a1332894f115f42a6baa46
SeanWilliamRutherford <swruther@ualberta.ca>
2023-01-19 10:17:29 -0700

    disable image optimization

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+    /* config options here */
+    images: {
+        unoptimized: true
+    }    
+  }
+  
+  module.exports = nextConfig
