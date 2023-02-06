674153f7b53ee1147cb3383afd7b9b661f7d4f17
SeanWilliamRutherford <swruther@ualberta.ca>
2023-02-06 10:01:07 -0700

    remove i18n form config, add lang=en to _doc.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,10 @@
  */
 
 const nextConfig = {
-    
+
     /* config options here */
     images: {
         unoptimized: true
-    },
-
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
     },
 
     trailingSlash: true,
